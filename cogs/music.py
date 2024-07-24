029f666dd102c824102871107eec6513ea3c1db3
Rubinart <k.ivanov19981@gmail.com>
2024-07-24 17:15:52 +0300

    Video search added

--- cogs/music.py
+++ cogs/music.py
@@ -15,8 +15,13 @@ class Music(commands.Cog):
                                "options": "-vn -filter:a volume=0.25"}
         self.yt_dl = YoutubeDL(self.YDL_OPTIONS)
 
+
     def search_yt(self, item):
-        pass
+        if item.startswith("https://www.youtube.com/"):
+            title = self.yt_dl.extract_info(item, download=False)["title"]
+            return {"source": item, "title": title}
+        search = self.yt_dl.extract_info(f"ytsearch:{item}", download=False)["entries"][0]
+        return {"source": search["formats"][0]["url"], "title": search["title"]}
 
 
     async def play_next(self):
