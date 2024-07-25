3d3661426658119c114fa4de53638f94ed6f98d2
Rubinart <k.ivanov19981@gmail.com>
2024-07-25 09:59:06 +0300

    Run engine added

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os, asyncio, discord, discord
 from discord.ext import commands
+from cogs.music import Music
 
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!")
+
+
+async def run_bot():
+    async with bot:
+        await bot.add_cog(Music(bot))
+        await bot.start(os.getenv("TOKEN"))
+        
+
+asyncio.run(run_bot())
