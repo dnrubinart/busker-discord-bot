a7e192ad39b4292f48c1f3c7a755409f614ae4af
Rubinart <k.ivanov19981@gmail.com>
2024-07-26 12:05:05 +0300

    Dotenv added

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import os, asyncio, discord, discord
+import os, asyncio, discord
 from discord.ext import commands
+from dotenv import load_dotenv
 from cogs.music import Music
 
 
+load_dotenv()
+
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 async def run_bot():
     async with bot:
         await bot.add_cog(Music(bot))
         await bot.start(os.getenv("TOKEN"))
-
 
 asyncio.run(run_bot())
