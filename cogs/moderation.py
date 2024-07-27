f6c91d42aa061983056d2b6798cd30eee519785d
Rubinart <k.ivanov19981@gmail.com>
2024-07-27 22:31:33 +0300

    Added ban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -4,8 +4,16 @@ from discord.ext import commands
 
 class Moderation(commands.Cog):
     
+
     @commands.has_any_role("Admin", "Moderator")
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         if reason is None:
             reason = "This user has been kicked by" + ctx.author.name
         await member.kick(reason=reason)
+
+
+    @commands.has_any_role("Admin", "Moderator")
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        if reason is None:
+            reason = "This user has been banned by" + ctx.author.name
+        await member.ban(reason=reason)
