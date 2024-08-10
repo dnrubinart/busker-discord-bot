91351aadd434eb976c14ad921fef8469e952b838
Rubinart <k.ivanov19981@gmail.com>
2024-08-10 10:15:34 +0300

    Added the draw method for generative art

--- cogs/art.py
+++ cogs/art.py
@@ -1,6 +1,10 @@
+import io
 import os
 
+import discord
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 from discord.ext import commands
+from PIL import Image
 from stability_sdk import client
 
 
@@ -11,3 +15,23 @@ class Art(commands.Cog):
             key=os.environ["STABILITY_API_KEY"],
             verbose=True,
         )
+
+    @commands.command()
+    async def draw(self, ctx, *, prompt):
+        message = await ctx.send("Drawing your image...")
+        generated_responses = self.stability_api.generate(prompt=prompt)
+
+        for response in generated_responses:
+            for artifact in response.artifacts:
+                if artifact.finish_reason == generation.FILTER:
+                    await ctx.send(
+                        "Could not process the request due to safety filters."
+                    )
+                elif artifact.type == generation.ARTIFACT_IMAGE:
+                    image = Image.open(io.BytesIO(artifact.binary))
+                    image_buffer = io.BytesIO(artifact.binary)
+                    image.save(image_buffer, format="PNG")
+                    image_buffer.seek(0)
+                    image_file = discord.File(image_buffer, filename="image.png")
+                    await message.edit(content=f"“{prompt}” \n")
+                    await ctx.send(file=image_file)
